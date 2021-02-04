b7f5c84e4134cfe47e11ad55715d0cadf1c9b042
Curro Mediavilla <59694404+Curro-Medi@users.noreply.github.com>
2021-02-04 10:05:39 +0100

    Add files via upload

---
+++ Examen29-01-2021/Examen29-01-2021/Devolver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen29_01_2021
+{
+    public partial class Devolver : Form
+    {
+
+        List<Cliente> Losclientes;
+        List<Videojuego> Losvideojuegos;
+        Form1 f1;
+        Cliente clienteseleccionado;
+
+
+        public Devolver(List<Cliente> clientes,List<Videojuego> videojuegos, Form1 f1)
+        {
+            InitializeComponent();
+            Losclientes = clientes;
+            Losvideojuegos = videojuegos;
+            this.f1 = f1;
+        }
+
+        private void Devolver_Load(object sender, EventArgs e)
+        {
+            foreach (Cliente c in Losclientes)
+            {
+                if(c.videojuegos.Count()!=0)
+                {
+                    ListaClientes.Items.Add(c);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            clienteseleccionado = (Cliente)ListaClientes.SelectedItem;
+
+            foreach(Videojuego cu in clienteseleccionado.videojuegos)
+            {
+                Losvideojuegos.Add(cu);
+            }
+
+            clienteseleccionado.videojuegos.Clear();
+            
+            this.Hide();
+            f1.Show();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            f1.Show();
+        }
+    }
+}
